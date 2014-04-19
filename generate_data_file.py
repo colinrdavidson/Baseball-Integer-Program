6c9315b0d96646f971558e88b72b11897cb3eee9
colinrdavidson <colinrdavidson@gmail.com>
2014-04-19 12:48:33 -0400

    Added in some checks for the csv headings.

--- generate_data_file.py
+++ generate_data_file.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 
 #A function to generate a data file for glpk
 #The input file is a csv where:
@@ -20,6 +21,53 @@ def generate_data_file(input_file, output_file):
       
       #Write each line of csv to data file
       for row in reader:
+        #Checks on heading names
+        if count == 0:
+          if row[0].lower() not in ["name", "player"]:
+            while True:
+              ans = input("The first column should be the player's name, but the heading is \"" + str(row[0]) + "\". Is it okay to proceed? [y/n]: ")
+              if ans.lower() == "y":
+                break
+              elif ans.lower() == "n":
+                print("Okay, aborting...")
+                sys.exit()
+              else:
+                print("INVALID INPUT")
+
+          if row[1].lower() not in ["position", "pos"]:
+            while True:
+              ans = input("The second column should be the player's position, but the heading is \"" + str(row[1]) + "\". Is it okay to proceed? [y/n]: ")
+              if ans.lower() == "y":
+                break
+              elif ans.lower() == "n":
+                print("Okay, aborting...")
+                sys.exit()
+              else:
+                print("INVALID INPUT")
+
+          if row[2].lower() not in ["salary", "sal"]:
+            while True:
+              ans = input("The third column should be the player's salary, but the heading is \"" + str(row[2]) + "\". Is it okay to proceed? [y/n]: ")
+              if ans.lower() == "y":
+                break
+              elif ans.lower() == "n":
+                print("Okay, aborting...")
+                sys.exit()
+              else:
+                print("INVALID INPUT")
+
+          if row[3].lower() not in ["points", "poi", "score"]:
+            while True:
+              ans = input("The fourth column should be the player's predicted points, but the heading is \"" + str(row[3]) + "\". Is it okay to proceed? [y/n]: ")
+              if ans.lower() == "y":
+                break
+              elif ans.lower() == "n":
+                print("Okay, aborting...")
+                sys.exit()
+              else:
+                print("INVALID INPUT")
+
+
         if count != 0:
           temp_row = [i for i in row]
           write_file.write("\n" + str(count) + " \"" + str(temp_row[0]) + "\" \"" + str(temp_row[1]) + "\" " + str(temp_row[2]) + " " + str(temp_row[3]))
